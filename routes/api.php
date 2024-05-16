3162e9878eb90de661db334885c8aa02cab3512d
datdv <datbst1102@gmail.com>
2024-05-16 23:18:54 +0700

    "Update api login app"

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,33 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('app/login', function (Request $request){
+
+    if ($request->get('username') == 'admin@iotsmart.vn' && $request->get('password') == '123456@') {
+        return response()->json([
+            'success' => true,
+            'token' => 'xxxxxxxxxxxxxxxxx123'
+        ]);
+    }
+
+   return response()->json([
+       'success' => false,
+       'message' => 'Thông tin đăng nhập không chính xác'
+   ]);
+});
+
+Route::post('app/check-login', function (Request $request){
+
+    if ($request->get('token') == 'xxxxxxxxxxxxxxxxx123') {
+        return response()->json([
+            'success' => true,
+        ]);
+    }
+
+    return response()->json([
+        'success' => false,
+        'message' => 'Token hết hạn'
+    ]);
+});
+
